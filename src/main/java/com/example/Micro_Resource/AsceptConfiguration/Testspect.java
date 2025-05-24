90b54ec5888b8e4c3bbaa44cf6c57bcc7612500d
unknown <revansiddawali70@gmail.com>
2025-05-25 02:05:34 +0530

    removed around aspect

--- src/main/java/com/example/Micro_Resource/AsceptConfiguration/Testspect.java
+++ src/main/java/com/example/Micro_Resource/AsceptConfiguration/Testspect.java
@@ -2,20 +2,32 @@ package com.example.Micro_Resource.AsceptConfiguration;
 
 import com.example.Micro_Resource.Entity.User;
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 
 @Aspect
 public class Testspect {
 
-
+/*
     @Around("execution(* com.example.Micro_Resource.service.UserDbCRUDRepositryService.getUserById(..)) || "+
-            "execution(* com.example.Micro_Resource.service.UserDbCRUDRepositryService.getFirstTenUsers(..))")
+            "execution(* com.example.Micro_Resource.service.UserDbCRUDRepositryService.getFirstTenUsers(..)) ||"+
+            "execution(* com.example.Micro_Resource.service.StudentService.java)")
     public User testspect(ProceedingJoinPoint joinPoint) throws Throwable {
        Object object = joinPoint.proceed();
        if( object != null ){
            return (User) object;
        } else
         return new User();
+    }*/
+
+    @Before("execution(* com.example.Micro_Resource.service.StudentService.java)")
+    public void getStudentBeforeAscpect(){
+        System.out.println("before srvice is invoked");
+    }
+    @After("execution(* com.example.Micro_Resource.service.StudentService.java)")
+    public void getStudentAfterAscpect(){
+        System.out.println("after srvice is invoked");
     }
 }
