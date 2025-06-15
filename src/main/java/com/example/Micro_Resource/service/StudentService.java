9e8b1521ce334df5c9ac24432ed19f5fcc705f37
unknown <revansiddawali70@gmail.com>
2025-06-16 01:33:02 +0530

    updated readme and added id check in student service

--- src/main/java/com/example/Micro_Resource/service/StudentService.java
+++ src/main/java/com/example/Micro_Resource/service/StudentService.java
@@ -1,17 +1,20 @@
 package com.example.Micro_Resource.service;
 
 import com.example.Micro_Resource.DTO.StudentDTO;
-import com.example.Micro_Resource.Entity.Student;
+import com.example.Micro_Resource.Entity.Members.Student;
 import com.example.Micro_Resource.EntityToDTOConverter.EntityToDTOConverter;
 import com.example.Micro_Resource.EntityToDTOConverterImpl.StudentEntityToBasicDTOConverter;
+import com.example.Micro_Resource.Enums.UserScope;
 import com.example.Micro_Resource.repositryInterface.StudentRepositry;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 /*
 extract logic of conveing DTO to entity and Entity to DTO in another package.
@@ -34,13 +37,14 @@ public class StudentService {
         this.passwordEncoder = passwordEncoder;
     }
 
-
+    @PreAuthorize("hasRole('CONTROLLER')")
+    @PostAuthorize("hasRole('CONTROLLER')")
     public StudentDTO SaveStudent(Student student) {
         if (studentRepositry != null) {
             try {
                 student.setPasswordHash(passwordEncoder.encode(student.getPasswordHash()));
                 Student savedStudent = studentRepositry.save(student);
-                return entityToDTOConverter.getDTOAsBasic(savedStudent).get();
+                return entityToDTOConverter.getDTOAsBasic(savedStudent, UserScope.CONTROLLER).get();
             } catch (Exception e) {
                 // log and handle exception
             }
@@ -49,22 +53,26 @@ public class StudentService {
             }
 
 
-
-
     public StudentDTO getStudentDTOById(int id) {
         if (studentRepositry != null) {
-            try {
-                Optional<Student> savedStudent = studentRepositry.findById(id);
-                return entityToDTOConverter.getDTOAsBasic(
-                        savedStudent.orElseGet(() -> null)
-                ).get();
+            if (id != 0) {
+                try {
+                    Optional<Student> savedStudent = studentRepositry.findById(id);
+                    // below is to fecth student info according to user scope
+                    GrantedAuthority authorities
+                            = new ArrayList<>(SecurityContextHolder.getContext().getAuthentication()
+                            .getAuthorities()).get(0);
+                    UserScope userScope = UserScope.getStudentScope(authorities.getAuthority());
+                    return entityToDTOConverter.getDTOAsBasic(
+                            savedStudent.orElseGet(() -> null), userScope).get();
 
-            } catch (Exception e) {
-                // add custom expection handler
+                } catch (Exception e) {
+                    // add custom expection handler
+                }
+
             }
-
         }
-        return new StudentDTO();
+       return new StudentDTO();
     }
 
     public List<StudentDTO> getAllStudents(Integer year) {
@@ -78,7 +86,8 @@ public class StudentService {
                 else {
                     studentIterable = studentRepositry.findAllByYear(year);
                 }
-                Optional<List<StudentDTO>> allDTOAsBasic = entityToDTOConverter.getAllDTOAsBasic(studentIterable.iterator());
+                // temporarily set basic
+                Optional<List<StudentDTO>> allDTOAsBasic = entityToDTOConverter.getAllDTOAsBasic(studentIterable.iterator(), UserScope.BASIC);
                 if (allDTOAsBasic.isPresent()) return allDTOAsBasic.get();
             } catch (Exception e) {
                 // implement exception handler and log the exception
@@ -87,4 +96,6 @@ public class StudentService {
         }
         return Collections.emptyList();
     }
+
+
 }
